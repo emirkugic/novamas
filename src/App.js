da05ccd814c0d9d4156c69db7b1f9ddd6d516820
emirkugic <emir.kugic@hotmail.com>
2025-05-26 08:47:28 +0200

    Setting up stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Homepage from "./pages/Home";
 import Post from "./pages/Post";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="/" element={<Homepage />} />
 				<Route path="/post/:id" element={<Post />} />
 			</Routes>
 		</Router>
